f2f0f9c4b19c6e8ab0d7d00762ae5389e72c419d
cmgsj <9107mgarcia@gmail.com>
2024-10-12 20:44:06 -0400

    make gen

--- pkg/gen/proto/blob/v1/blob.pb.gw.go
+++ pkg/gen/proto/blob/v1/blob.pb.gw.go
@@ -123,11 +123,7 @@ func request_BlobService_WriteBlob_0(ctx context.Context, marshaler runtime.Mars
 	var protoReq WriteBlobRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -157,11 +153,7 @@ func local_request_BlobService_WriteBlob_0(ctx context.Context, marshaler runtim
 	var protoReq WriteBlobRequest
 	var metadata runtime.ServerMetadata
 
-	newReader, berr := utilities.IOReaderFactory(req.Body)
-	if berr != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
-	}
-	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -243,6 +235,7 @@ func local_request_BlobService_RemoveBlob_0(ctx context.Context, marshaler runti
 // UnaryRPC     :call BlobServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterBlobServiceHandlerFromEndpoint instead.
+// GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterBlobServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server BlobServiceServer) error {
 
 	mux.Handle("GET", pattern_BlobService_ListBlobs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
@@ -351,21 +344,21 @@ func RegisterBlobServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux
 // RegisterBlobServiceHandlerFromEndpoint is same as RegisterBlobServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterBlobServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
@@ -383,7 +376,7 @@ func RegisterBlobServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn
 // to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "BlobServiceClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "BlobServiceClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
-// "BlobServiceClient" to call the correct interceptors.
+// "BlobServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterBlobServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client BlobServiceClient) error {
 
 	mux.Handle("GET", pattern_BlobService_ListBlobs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
